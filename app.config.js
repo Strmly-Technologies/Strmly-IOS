fe42d9ad1dc1650079e5917b78e9f73fb65625a7
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-10-18 11:57:58 +0530

    app.config.js removed ios

--- app.config.js
+++ app.config.js
@@ -6,66 +6,15 @@ export default {
     slug: "strmly",
     scheme: "strmly",
     owner: "strmly-technologies",
-    version: "1.0.8",
-    orientation: "portrait",
-    icon: "./assets/images/logo3.jpg",
-    userInterfaceStyle: "dark",
+    version: "1.0.8", // purely for JS metadata, not native
     newArchEnabled: true,
-    ios: {
-      bundleIdentifier: "com.anonymous.strmly",
-      buildNumber: "9",
-      supportsTablet: true,
-      infoPlist: {
-        ITSAppUsesNonExemptEncryption: false,
-        UIBackgroundModes: [],
-        NSPhotoLibraryUsageDescription: "Strmly needs access to your photo library to let you upload videos, select custom thumbnails for your content, and attach photos as proof when reporting issues. For example, you can upload videos from your gallery, choose thumbnail images for your posts, and attach screenshots when reporting inappropriate content."
-      }
-    },
-    android: {
-      versionCode: 3,
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/logo3.jpg",
-        backgroundColor: "#000"
-      },
-      package: "com.anonymous.strmly",
-      softwareKeyboardLayoutMode: "pan",
-      edgeToEdgeEnabled: true
-    },
-    web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/logo3.jpg"
-    },
     plugins: [
       "expo-router",
       "expo-secure-store",
-      [
-        "expo-splash-screen",
-        {
-          image: "./assets/images/logo3.jpg",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#000"
-        }
-      ],
-      [
-        "expo-video",
-        {
-          supportsBackgroundPlayback: false,
-          supportsPictureInPicture: true
-        }
-      ],
-      [
-        "react-native-iap",
-        {
-          "googlePlayPackageName": "com.anonymous.strmly"
-        }
-      ],
-      "expo-web-browser"
+      ["expo-video", { supportsBackgroundPlayback: false, supportsPictureInPicture: false }],
+      ["react-native-iap", { googlePlayPackageName: "com.anonymous.strmly" }],
+      "expo-web-browser",
     ],
-    experiments: {
-      typedRoutes: true
-    },
     extra: {
       BACKEND_API_URL: process.env.BACKEND_API_URL,
       EXPO_PUBLIC_BACKEND_API_URL: process.env.EXPO_PUBLIC_BACKEND_API_URL,
@@ -73,8 +22,8 @@ export default {
       googleClientIdIOS: process.env.GOOGLE_CLIENT_ID_IOS,
       googleClientIdWeb: process.env.GOOGLE_CLIENT_ID_WEB,
       eas: {
-        projectId: "d455cc31-c9ac-409d-92de-b847919939d4"
-      }
-    }
-  }
+        projectId: "d455cc31-c9ac-409d-92de-b847919939d4",
+      },
+    },
+  },
 };
